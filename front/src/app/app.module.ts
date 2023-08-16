9e035090850b3401488850821f7853e7e7c0981c
Wallace Wagner <wallace20130@gmail.com>
2023-08-16 12:26:07 -0300

    Tela inicial

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ComponentesComponent,
+    CabecalhoComponent,
+    RodapeComponent,
     HomeComponent
   ],
   imports: [
