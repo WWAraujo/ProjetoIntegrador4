8d1e58bffb145641196a4e3d8dc8c440cd7acbdf
Semaías Lima <semaias_oliveira@hotmail.com>
2023-08-22 21:29:36 -0300

    Tela de cadastro criada

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/logins/login/login.component';
 import { ListarUsuarioComponent } from './componentes/usuario/listar-usuario/listar-usuario.component';
+import { CadastroUsuarioComponent } from './componentes/usuario/cadastro-usuario/cadastro-usuario.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'listarUsuario',
     component: ListarUsuarioComponent,
+  },
+  {
+    path: 'CadastrarUsuario',
+    component: CadastroUsuarioComponent,
   }
 ];
 
--- front/src/app/componentes/usuario/cadastro-usuario/cadastro-usuario.component.ts
+++ front/src/app/componentes/usuario/cadastro-usuario/cadastro-usuario.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroUsuarioComponent implements OnInit {
 
+  user: User = {
+    nome: '',
+    cpf: '',
+    email: '',
+    grupo: 'ADM',
+    senha: ''
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  confirmarSenha: string = '';
+
+  cadastrarUsuario() {
+    // Aqui você pode implementar a lógica para cadastrar o usuário
+    // Verifique se o e-mail não está duplicado antes de realizar o cadastro
+    if (this.user.email === 'email@existente.com') {
+      alert('Este e-mail já está cadastrado. Escolha outro.');
+    } else {
+      console.log('Usuário cadastrado com sucesso:', this.user);
+    }
+  }
 }
+
+export interface User {
+  nome: string;
+  cpf: string;
+  email: string;
+  grupo: string; // Pode ser 'ADM' ou 'ESTOQUISTA'
+  senha: string;
+}
+
+
