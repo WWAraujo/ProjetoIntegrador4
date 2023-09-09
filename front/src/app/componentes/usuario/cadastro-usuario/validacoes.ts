b5f023afbddc9f4546db21400eef0c6293d90f6a
Micael Oliveira <micaelds98@outlook.com>
2023-09-09 04:24:12 -0300

    att validação do cpf

--- front/src/app/componentes/usuario/cadastro-usuario/validacoes.ts
+++ front/src/app/componentes/usuario/cadastro-usuario/validacoes.ts
@@ -4,50 +4,57 @@ export class Validacoes {
   static ValidaCPF(controle: AbstractControl): { cpfInvalido: true } | null {
     const cpf = controle.value;
 
-    let soma: number = 0;
+    if (!Validacoes.validaCPF(cpf)) {
+      return { cpfInvalido: true };
+    }
+
+    return null;
+  }
+
+  private static validaCPF(cpf: string): boolean {
+    cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
+
+    if (cpf.length !== 11) {
+      return false; // CPF deve ter 11 dígitos
+    }
+
+    if (/^(\d)\1{10}$/.test(cpf)) {
+      return false; // CPF com todos os dígitos iguais é inválido
+    }
+
+    let soma = 0;
     let resto: number;
-    let valido: boolean;
 
-    const regex = new RegExp('[0-9]{11}');
-
-    if (
-      cpf == '00000000000' ||
-      cpf == '11111111111' ||
-      cpf == '22222222222' ||
-      cpf == '33333333333' ||
-      cpf == '44444444444' ||
-      cpf == '55555555555' ||
-      cpf == '66666666666' ||
-      cpf == '77777777777' ||
-      cpf == '88888888888' ||
-      cpf == '99999999999' ||
-      !regex.test(cpf)
-    ) {
-      valido = false;
-    } else {
-      for (let i = 1; i <= 9; i++) {
-        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-      }
-      resto = (soma * 10) % 11;
-
-      if (resto == 10 || resto == 11) resto = 0;
-      if (resto != parseInt(cpf.substring(9, 10))) valido = false;
-
-      soma = 0;
-      for (let i = 1; i <= 10; i++) {
-        soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-      }
-      resto = (soma * 10) % 11;
-
-      if (resto == 10 || resto == 11) resto = 0;
-      if (resto != parseInt(cpf.substring(10, 11))) valido = false;
-      valido = true;
+    for (let i = 1; i <= 9; i++) {
+      soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
 
-    if (valido) {
-      return null;
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) {
+      resto = 0;
     }
 
-    return { cpfInvalido: true };
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+      return false;
+    }
+
+    soma = 0;
+
+    for (let i = 1; i <= 10; i++) {
+      soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+      return false;
+    }
+
+    return true;
   }
 }
