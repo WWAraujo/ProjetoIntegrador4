dc9506b16d9bf57f5df14bfc8050e17d2fd484ab
Wallace Wagner <wallace20130@gmail.com>
2023-11-09 00:58:20 -0300

    Auto stash before rebase of "refs/heads/feature/carrinho"

---
+++ backend/src/main/java/edu/senac/backend/vendas/ListarPedidos.java
@@ -0,0 +1,14 @@
+package edu.senac.backend.vendas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListarPedidos {
+
+    public List<PedidosRecord> listarPedido(Long cliente){
+        List<PedidosRecord> list = new ArrayList<>();
+
+
+        return list;
+    }
+}
